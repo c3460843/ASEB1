1603d323e0671f53928e5c32cdec3dddd27cf658
Thomas Mungovin <T.Mungovin8658@student.leedsbeckett.ac.uk>
2018-03-03 17:18:33 +0000

    second commit

--- Data Analysis Software Part 1/Form1.cs
+++ Data Analysis Software Part 1/Form1.cs
@@ -22,6 +22,16 @@ namespace Data_Analysis_Software_Part_1
         double altitudeTotal = 0;
         int heartRateTotal = 0;
         int powerTotal = 0;
+        int version;
+        bool sModeSpeed;
+        bool sModeCadence;
+        bool sModeAltitude;
+        bool sModePower;
+        bool sModePowerLeftRightBalance;
+        bool sModePowerPedallingIndex;
+        bool sModeHRCC;
+        bool sModeUnitStandard;
+        bool sModeAirPressure;
         List<string> timeList = new List<string>();
         List<double> speedList = new List<double>();
         List<int> cadenceList = new List<int>();
@@ -42,6 +52,37 @@ namespace Data_Analysis_Software_Part_1
             StreamReader file = new StreamReader(myStream);
             while ((line = file.ReadLine()) != null)
             {
+                if (line.Contains("Version"))
+                {
+                    String[] lineArray = line.Split('=');
+                    version = Int32.Parse(lineArray[1]);
+                }
+
+                if (line.Contains("SMode"))
+                {
+                    String[] lineArray = line.Split('=');
+                    char[] sModeChars = lineArray[1].ToCharArray();
+                    if (version >= 106)
+                    {
+                        if (sModeChars[0] == '0') { sModeSpeed = false; }                 else if (sModeChars[0] == '1') { sModeSpeed = true; }
+                        if (sModeChars[1] == '0') { sModeCadence = false; }               else if (sModeChars[1] == '1') { sModeCadence = true; }
+                        if (sModeChars[2] == '0') { sModeAltitude = false; }              else if (sModeChars[2] == '1') { sModeAltitude = true; }
+                        if (sModeChars[3] == '0') { sModePower = false; }                 else if (sModeChars[3] == '1') { sModePower = true; }
+                        if (sModeChars[4] == '0') { sModePowerLeftRightBalance = false; } else if (sModeChars[4] == '1') { sModePowerLeftRightBalance = true; }
+                        if (sModeChars[5] == '0') { sModePowerPedallingIndex = false; }   else if (sModeChars[5] == '1') { sModePowerPedallingIndex = true; }
+                        if (sModeChars[6] == '0') { sModeHRCC = false; }                  else if (sModeChars[6] == '1') { sModeHRCC = true; }
+                        if (sModeChars[7] == '0') { sModeUnitStandard = false; }          else if (sModeChars[7] == '1') { sModeUnitStandard = true; }
+                    }
+                    if (version >= 107)
+                    {
+                        if (sModeChars[8] == 0) { sModeAirPressure = false; }           else if (sModeChars[8] == 1) { sModeAirPressure = true; }
+                    }
+                    Console.WriteLine("Version="+version);
+                    Console.WriteLine("SMode="+sModeChars[0].ToString() + sModeChars[1].ToString() + sModeChars[2].ToString() +
+                                      sModeChars[3].ToString() + sModeChars[4].ToString() + sModeChars[5].ToString() +
+                                      sModeChars[6].ToString() + sModeChars[7].ToString());
+                }
+
                 if (line.Contains("Date"))
                 {
                     String[] lineArray = line.Split('=');
@@ -74,7 +115,7 @@ namespace Data_Analysis_Software_Part_1
                         String[] lineArray = line.Split();
                         timeList.Add(timeString);
                         heartRateList.Add(Int32.Parse(lineArray[0]));
-                        speedList.Add(Int32.Parse(lineArray[1]));
+                        if (sModeSpeed == true) { speedList.Add(Int32.Parse(lineArray[1])); }
                         cadenceList.Add(Int32.Parse(lineArray[2]));
                         altitudeList.Add(Int32.Parse(lineArray[3]));
                         powerList.Add(Int32.Parse(lineArray[4]));
@@ -92,7 +133,15 @@ namespace Data_Analysis_Software_Part_1
         {
             for (int i = 0; i < timeList.Count; i++)
             {
-            dataGridView.Rows.Add(timeList[i], heartRateList[i], speedList[i], cadenceList[i], altitudeList[i], powerList[i], pBBIList[i]);
+                string speedValue="";
+                string cadenceValue="";
+                string altitudeValue="";
+                int powerValue=0;
+                if (sModeSpeed==true)    { speedValue = (speedList[i]/10).ToString(); }
+                if (sModeCadence==true)  { cadenceValue = cadenceList[i].ToString(); }
+                if (sModeAltitude==true) { altitudeValue = altitudeList[i].ToString(); }
+                if (sModePower==true)    { powerValue = powerList[i]; }
+                dataGridView.Rows.Add(timeList[i], heartRateList[i], speedValue, cadenceValue, altitudeValue, powerList[i], powerValue);
             }
         }
 
@@ -104,56 +153,64 @@ namespace Data_Analysis_Software_Part_1
             heartRateTotal = 0;
             powerTotal = 0;
 
-            foreach (int heartRateInt in heartRateList)
+            try
             {
-                heartRateTotal += heartRateInt;
-            }
-            double heartRateAverage = heartRateTotal / heartRateList.Count;
-            heartRateAverageLabel.Text = heartRateAverage.ToString() + "bpm";
 
-            foreach (double speedDouble in speedList)
-            {
-                speedTotal += speedDouble;
-            }
-            double speedAverage = speedTotal / speedList.Count;
-            speedAverage = Math.Round(speedAverage, 1);
-            if (measurementTrackBar.Value == 0)
-            {
-                speedAverageLabel.Text = (speedAverage / 10).ToString() + "km/h";
-            }
-            else if (measurementTrackBar.Value == 1)
-            {
-                speedAverageLabel.Text = (speedAverage / 10).ToString() + "mph";
-            }
+                foreach (int heartRateInt in heartRateList)
+                {
+                    heartRateTotal += heartRateInt;
+                }
+                double heartRateAverage = heartRateTotal / heartRateList.Count;
+                heartRateAverageLabel.Text = heartRateAverage.ToString() + "bpm";
 
-            foreach (int cadenceInt in cadenceList)
-            {
-                cadenceTotal += cadenceInt;
-            }
-            double cadenceAverage = cadenceTotal / cadenceList.Count;
-            cadenceAverageLabel.Text = cadenceAverage.ToString() + "rpm";
+                foreach (double speedDouble in speedList)
+                {
+                    speedTotal += speedDouble;
+                }
+                double speedAverage = speedTotal / speedList.Count;
+                speedAverage = Math.Round(speedAverage, 1);
+                if (measurementTrackBar.Value == 0)
+                {
+                    speedAverageLabel.Text = (speedAverage / 10).ToString() + "km/h";
+                }
+                else if (measurementTrackBar.Value == 1)
+                {
+                    speedAverageLabel.Text = (speedAverage / 10).ToString() + "mph";
+                }
 
-            foreach (double altitudeDouble in altitudeList)
-            {
-                altitudeTotal += altitudeDouble;
-            }
-            double altitudeAverage = altitudeTotal / altitudeList.Count;
-            altitudeAverage = Math.Round(altitudeAverage, 2);
-            if (measurementTrackBar.Value == 0)
-            {
-                altitudeAverageLabel.Text = altitudeAverage.ToString() + "m";
-            }
-            else if (measurementTrackBar.Value == 1)
-            {
-                altitudeAverageLabel.Text = altitudeAverage.ToString() + "ft";
-            }
+                foreach (int cadenceInt in cadenceList)
+                {
+                    cadenceTotal += cadenceInt;
+                }
+                double cadenceAverage = cadenceTotal / cadenceList.Count;
+                cadenceAverageLabel.Text = cadenceAverage.ToString() + "rpm";
 
-            foreach (int powerInt in powerList)
-            {
-                powerTotal += powerInt;
+                foreach (double altitudeDouble in altitudeList)
+                {
+                    altitudeTotal += altitudeDouble;
+                }
+                double altitudeAverage = altitudeTotal / altitudeList.Count;
+                altitudeAverage = Math.Round(altitudeAverage, 2);
+                if (measurementTrackBar.Value == 0)
+                {
+                    altitudeAverageLabel.Text = altitudeAverage.ToString() + "m";
+                }
+                else if (measurementTrackBar.Value == 1)
+                {
+                    altitudeAverageLabel.Text = altitudeAverage.ToString() + "ft";
+                }
+
+                foreach (int powerInt in powerList)
+                {
+                    powerTotal += powerInt;
+                }
+                double powerAverage = powerTotal / powerList.Count;
+                powerAverageLabel.Text = powerAverage.ToString() + "Watts";
             }
-            double powerAverage = powerTotal / powerList.Count;
-            powerAverageLabel.Text = powerAverage.ToString() + "Watts";
+            catch (System.DivideByZeroException)
+            {
+
+            }
         }
 
         private void openToolStripMenuItem_Click(object sender, EventArgs e)
